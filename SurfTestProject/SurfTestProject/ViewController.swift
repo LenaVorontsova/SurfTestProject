a6b61967a507540dd820b9689abdce4cc8a8437d
Elena Vorontsova <lena.voroncova26@yandex.ru>
2023-02-08 18:04:57 +0400

    add: create a new project

---
+++ SurfTestProject/SurfTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfTestProject
+//
+//  Created by Lena Vorontsova on 08.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
